f093808121cdfc6dce34c4532620f36b9acf03a3
Ndegwa Jackson <128777440+jackdevs-lab@users.noreply.github.com>
2025-11-12 01:30:04 +0300

    added disconnect.html

--- api/__init__.py
+++ api/__init__.py
@@ -2,12 +2,16 @@
 from flask import Flask
 import sys
 from pathlib import Path
-
+from flask import Flask, render_template
 # Fix path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
+app = Flask(__name__, template_folder='../templates')  # This is correct
 
 # Import your app from root (now it's a module)
 from app import app
+@app.route('/disconnect')
+def disconnect():
+        return render_template('disconnect.html')
 app = app  # Optional, but clear
 application = app
 # Vercel looks for `application` or `app`
